b129413a0a98b10ab4d2c606b07537145b98a63c
Anna <ann.sxd@example.com>
2022-12-18 09:46:28 +0300

    Задача с перемешванием списка

--- 03_shuffling_the_list.py
+++ 03_shuffling_the_list.py
@@ -1,14 +1,14 @@
 # Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE
-
+n = int(input('Введите число до какого нужны случайные числа: '))
 import random
 
 my_list = []
-for i in range(10):
+for i in range(n):
     my_list.append(i)
 print(my_list)
 
 for i in range(10):
-    rand_index = random.randint(0, 9)    # взяла метод .randint чтобы получить рандомный индекс в пределах последовательности. 
+    rand_index = random.randint(0, len(my_list)-1)    # взяла метод .randint чтобы получить рандомный индекс в пределах последовательности. 
     temp = my_list[i]
     my_list[i] = my_list[rand_index]
     my_list[rand_index] = temp
